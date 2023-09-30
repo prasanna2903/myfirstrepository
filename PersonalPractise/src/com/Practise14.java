682268f9d1706c39befed0dea01cca5314e4a78c
pravenka <pravenka@LIN25008039.corp.capgemini.com>
2023-09-30 15:46:59 +0530

    commit

--- PersonalPractise/src/com/Practise14.java
+++ PersonalPractise/src/com/Practise14.java
@@ -3,8 +3,15 @@ package com;
 public class Practise14 {
 
 	public static void main(String[] args) {
+		String s="wqer1235";
 		
-	
+		int sum = 0;
+		for(char c:s.toCharArray())
+			if(Character.isDigit(c)) {
+				int i = Integer.parseInt(String.valueOf(c));
+				sum+=i;
+			}
+		System.out.println(sum);
 	
 	}
 
